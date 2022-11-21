d87f71c34ab4278c3df457e921aa151d7c1d7b63
MrMatterzz <jakeiswin25@gmail.com>
2022-11-21 19:17:52 +0200

    Recreated the main window from the old project

--- PTOJavaFXProject/src/application/Main.java
+++ PTOJavaFXProject/src/application/Main.java
@@ -2,7 +2,9 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,11 +13,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,1440,900);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Image appIcon = new Image("C:\\Users\\MISHA\\Desktop\\Work files\\MasterProject\\PTOJavaFXProject\\src\\application\\app.png");
+			primaryStage.getIcons().add(appIcon);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Public Traffic Organizer");
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- PTOJavaFXProject/src/module-info.java
+++ PTOJavaFXProject/src/module-info.java
@@ -1,6 +1,7 @@
 module PTOJavaFXProject {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
